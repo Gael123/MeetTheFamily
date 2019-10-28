a2008cf3339ddaeef9db823b406880bb5d20f52f
Gael Jerop <gael.jerop@yahoo.com>
2019-10-28 16:27:10 +1100

    created person controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,47 @@
 class PeopleController < ApplicationController
+  def index
+    @people = Person.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @people }
+    end
+  end
+
+  def show
+    @person = Person.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @people }
+    end
+  end
+
+  def create
+    @person = Person.new(person_params)
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to @person, notice: 'Person was created.' }
+        format.json { render json: @person, status: :created, location: @person }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @person = Person.new
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :second_name, :date_of_birth)
+  end
 end
