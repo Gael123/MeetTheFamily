152a011b2d13a87ebb82963d3bbf910c437840da
Gael Jerop <gael.jerop@yahoo.com>
2019-10-28 22:15:16 +1100

    did controllers and corresponding templates

--- app/controllers/relationship_types_controller.rb
+++ app/controllers/relationship_types_controller.rb
@@ -1,2 +1,33 @@
 class RelationshipTypesController < ApplicationController
+  def index
+    @relationship_types = RelationshipType.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @relationship_type }
+    end
+  end
+
+ def show
+  @relationship_types = RelationshipType.find(params[:id])
+
+  respond_to do | format|
+    format.html
+    format.json { render json: @relationship_type }
+  end
+ end
+
+ def new
+  @relationship_type = RelationshipType.new
+
+  respond_to do |format|
+    format.html
+    format.json { render json: @relationship_type }
+  end
+ end
+
+ def edit
+  @relationship_type = RelationshipType.find(params[:id])
+
+ end
+
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,44 @@
 class RelationshipsController < ApplicationController
+  before_filter do
+    @person = Person.find(params[:person_id])
+  end
+
+  def index
+    @relationships = @person.relationships
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @relationships }
+    end
+  end
+
+  def show
+    @relationship = Relationship.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @relationship }
+    end
+  end
+
+  def new
+    @relationship = @person.relationships.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @relationship }
+    end
+  end
+
+  def create
+    @relationship = @person.relationships.new(params[:relationship])
+
+    respond_to do |format|
+      if @relationship.save
+        format.html { redirect_to person_relationship_path(@person, @relationship), notice: 'Relationship was successfully created.' }
+        format.json { render json: @relationship, status: :created, location: @relationship }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @relationship.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
